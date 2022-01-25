f70f6cf083ce3642777ee36d8f1249ddda6831b5
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-01-25 14:26:30 +0100

    Find project by identifier.

--- spring/src/main/java/com/nv/ppmtool/services/ProjectService.java
+++ spring/src/main/java/com/nv/ppmtool/services/ProjectService.java
@@ -18,8 +18,20 @@ public class ProjectService {
             return projectRepository.save(project);
         } catch (Exception e) {
             throw new ProjectIdException(
-                    String.format("Project ID '%s' already exists.",
+                    String.format("Project ID '%s' already exists",
                             project.getProjectIdentifier().toUpperCase()));
         }
+    }
+
+    public Project findProjectByIdentifier(String projectIdentifier) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
+
+        if (project == null)
+            throw new ProjectIdException(
+                    String.format("Project ID '%s' doesn't exist",
+                            projectIdentifier.toUpperCase()));
+
+        return project;
     }
 }
--- spring/src/main/java/com/nv/ppmtool/web/ProjectController.java
+++ spring/src/main/java/com/nv/ppmtool/web/ProjectController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -30,5 +27,13 @@ public class ProjectController {
         Project savedProject = projectService.saveOrUpdateProject(project);
 
         return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectIdentifier}")
+    public ResponseEntity<?> getProjectByIdentifier(@PathVariable String projectIdentifier) {
+
+        Project project = projectService.findProjectByIdentifier(projectIdentifier);
+
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 }
