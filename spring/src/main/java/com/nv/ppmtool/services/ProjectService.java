ba81288cd3de30d8fe87bc370b30bbf7111b4308
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-01-25 14:00:14 +0100

    Custom project identifier exception handling.

--- spring/src/main/java/com/nv/ppmtool/services/ProjectService.java
+++ spring/src/main/java/com/nv/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.nv.ppmtool.services;
 
 import com.nv.ppmtool.domain.Project;
+import com.nv.ppmtool.exceptions.ProjectIdException;
 import com.nv.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,13 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project) {
 
-        // logic
-
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException(
+                    String.format("Project ID '%s' already exists.",
+                            project.getProjectIdentifier().toUpperCase()));
+        }
     }
 }
