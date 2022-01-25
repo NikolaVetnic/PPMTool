5b88299d5ced080c6545db63fa19f5e830ec603f
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-01-25 21:14:07 +0100

    First React class-based component.

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the Dashboard</h1>
+                <p>Enjoy!</p>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
