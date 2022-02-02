ef4f1dae726f3e46f93d72d422ca015c49d8a8c0
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-02-03 00:41:30 +0100

    Finish AddProjectTask action, handle errors.

--- client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -22,6 +22,14 @@ class AddProjectTask extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors,
+            });
+        }
+    }
+
     onChange(e) {
         this.setState({ [e.target.name]: e.target.value });
     }
@@ -47,6 +55,7 @@ class AddProjectTask extends Component {
 
     render() {
         const { id } = this.props.match.params;
+        const { errors } = this.state;
         return (
             <div className="add-PBI">
                 <div className="container">
@@ -68,12 +77,22 @@ class AddProjectTask extends Component {
                                 <div className="form-group">
                                     <input
                                         type="text"
-                                        className="form-control form-control-lg"
+                                        className={classnames(
+                                            "form-control form-control-lg",
+                                            {
+                                                "is-invalid": errors.summary,
+                                            }
+                                        )}
                                         name="summary"
                                         placeholder="Project Task summary"
                                         value={this.state.summary}
                                         onChange={this.onChange}
                                     />
+                                    {errors.summary && (
+                                        <div className="invalid-feedback">
+                                            {errors.summary}
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <textarea
@@ -148,4 +167,4 @@ const mapStateToProps = (state) => ({
     errors: state.errors,
 });
 
-export default connect(null, { addProjectTask })(AddProjectTask);
+export default connect(mapStateToProps, { addProjectTask })(AddProjectTask);
