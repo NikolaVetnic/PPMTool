2ff80eaaadf18f7b3a24353c031e515285765e43
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-02-08 22:51:49 +0100

    Create User object, Validation, Repository, Service.

--- spring/src/main/java/com/nv/ppmtool/domain/User.java
+++ spring/src/main/java/com/nv/ppmtool/domain/User.java
@@ -1,4 +1,61 @@
 package com.nv.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username field is required")
+    private String username;
+
+    @NotBlank(message = "Please enter your full name")
+    private String fullName;
+
+    @NotBlank(message = "Password field is required")
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @Column(updatable = false)
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date created_At;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_At;
+
+    // OneToMany with Project
+
+    public User() { }
+
+    @PrePersist protected void onCreate()   { this.created_At = new Date(); }
+    @PreUpdate protected void onUpdate()    { this.updated_At = new Date(); }
+
+    public Long getId()                 { return id;                }
+    public String getUsername()         { return username;          }
+    public String getFullName()         { return fullName;          }
+    public String getPassword()         { return password;          }
+    public String getConfirmPassword()  { return confirmPassword;   }
+    public Date getCreated_At()         { return created_At;        }
+    public Date getUpdated_At()         { return updated_At;        }
+
+    public void setId(Long id)                              { this.id = id;                             }
+    public void setUsername(String username)                { this.username = username;                 }
+    public void setFullName(String fullName)                { this.fullName = fullName;                 }
+    public void setPassword(String password)                { this.password = password;                 }
+    public void setConfirmPassword(String confirmPassword)  { this.confirmPassword = confirmPassword;   }
+    public void setCreated_At(Date created_At)              { this.created_At = created_At;             }
+    public void setUpdated_At(Date updated_At)              { this.updated_At = updated_At;             }
 }
---
+++ spring/src/main/java/com/nv/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nv.ppmtool.repositories;
+
+import com.nv.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
